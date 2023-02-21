9fe309a28906e67278a1aeeb7c5a015879972fa4
David Nyamu <nyamu007@yahoo.com>
2023-02-21 08:27:31 -0800

    yes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,17 @@
 /**
  * print_sign - prints the sign of a number
  * @n: The number to be checked
- * Return: 1 for positive num, -1for negative num or 0 for anything else
+ * Return: 1 for positive num, -1 for negative num or0 for anything else
  */
 
 int print_sign(int n)
 
 {
+
 	if (n > 0)
 	{
 	_putchar(43);
-	return(1);
+	return (1);
 	}
 	else if (n < 0)
 	{
@@ -20,7 +21,7 @@ int print_sign(int n)
 	}
 	else
 	{
-	_putchar (48);
+	_putchar(48);
 	return (0);
 	}
 }
